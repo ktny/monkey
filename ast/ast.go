a07367fd5b52ef1518d5051d447136fed30b6363
ktny <katsunori.t10@gmail.com>
2021-01-02 15:33:50 +0900

    fix

--- ast/ast.go
+++ ast/ast.go
@@ -1,27 +1,50 @@
 package ast
 
 import (
+	"bytes"
+
 	"github.com/ktny/monkey/token"
 )
 
+// Node ASTの最も基本的な単位。Token1つであり、TokenLiteralを持つ
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
+// Statement 文。Nodeの集まり
 type Statement interface {
 	Node
 	statementNode()
 }
 
+// Expression 式。Nodeの集まり
 type Expression interface {
 	Node
 	expressionNode()
 }
 
-type Program struct {
-	Statements []Statement
+// Identifier 識別子。Token1つからなり、Valueを持つ。変数
+// Expression I/F
+// 	expressionNode()
+// Node I/F
+// 	TokenLiteral()
+// 	String()
+type Identifier struct {
+	Token token.Token
+	Value string
 }
 
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
+
+// LetStatement let文。
+// Statement I/F
+// 	statementNode()
+// Node I/F
+// 	TokenLiteral()
+// 	String()
 type LetStatement struct {
 	Token token.Token // letトークン
 	Name  *Identifier
@@ -30,7 +53,27 @@ type LetStatement struct {
 
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
 
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
+}
+
+// ReturnStatement return文。
+// Statement I/F
+// 	statementNode()
+// Node I/F
+// 	TokenLiteral()
+// 	String()
 type ReturnStatement struct {
 	Token       token.Token // returnトークン
 	ReturnValue Expression
@@ -38,14 +81,46 @@ type ReturnStatement struct {
 
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
 
-type Identifier struct {
-	Token token.Token
-	Value string
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
 }
 
-func (i *Identifier) expressionNode()      {}
-func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+// ExpressionStatement 式文。
+// Statement I/F
+// 	statementNode()
+// Node I/F
+// 	TokenLiteral()
+// 	String()
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+
+// Program プログラム。複数の式からなる
+// Node I/F
+// 	TokenLiteral()
+// 	String()
+type Program struct {
+	Statements []Statement
+}
 
 // TokenLiteral 先頭の文のTokenLiteralを返す
 func (p *Program) TokenLiteral() string {
@@ -54,4 +129,15 @@ func (p *Program) TokenLiteral() string {
 	} else {
 		return ""
 	}
+}
+
+// Programが持つ文すべてのStringを返す
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
 }
