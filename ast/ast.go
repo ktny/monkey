5003462710a5cd59267caee64cb48309d1895c77
ktny <katsunori.t10@gmail.com>
2021-01-03 10:58:31 +0900

    add fn parser

--- ast/ast.go
+++ ast/ast.go
@@ -2,6 +2,7 @@ package ast
 
 import (
 	"bytes"
+	"strings"
 
 	"github.com/ktny/monkey/token"
 )
@@ -138,6 +139,36 @@ func (ie *IfExpression) String() string {
 		out.WriteString(ie.Alternative.String())
 	}
 
+	return out.String()
+}
+
+// FunctionLiteral 関数リテラル
+// Expression I/F
+// 	expressionNode()
+// Node I/F
+// 	TokenLiteral()
+// 	String()
+type FunctionLiteral struct {
+	Token      token.Token // fnトークン
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode()      {}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
 	return out.String()
 }
 
