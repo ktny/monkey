959dcee5afadc22944445a6e3ca365afdbb48024
ktny <katsunori.t10@gmail.com>
2021-01-04 22:25:35 +0900

    add environment

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -12,8 +12,9 @@ func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
 	program := p.ParseProgram()
+	env := object.NewEnvironment()
 
-	return Eval(program)
+	return Eval(program, env)
 }
 
 func TestEvalIntegerExpression(t *testing.T) {
@@ -236,22 +237,22 @@ func TestErrorHandling(t *testing.T) {
 			"if (10 > 1) { true + false; }",
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
-		// 		{
-		// 			`
-		// if (10 > 1) {
-		//   if (10 > 1) {
-		//     return true + false;
-		//   }
+		{
+			`
+		if (10 > 1) {
+		  if (10 > 1) {
+		    return true + false;
+		  }
 
-		//   return 1;
-		// }
-		// `,
-		// 			"unknown operator: BOOLEAN + BOOLEAN",
-		// 		},
-		// 		{
-		// 			"foobar",
-		// 			"identifier not found: foobar",
-		// 		},
+		  return 1;
+		}
+		`,
+			"unknown operator: BOOLEAN + BOOLEAN",
+		},
+		{
+			"foobar",
+			"identifier not found: foobar",
+		},
 	}
 
 	for _, tt := range tests {
@@ -266,5 +267,21 @@ func TestErrorHandling(t *testing.T) {
 		if errObj.Message != tt.expectedMessage {
 			t.Errorf("wrong error message. expected=%q, got=%q", tt.expectedMessage, errObj.Message)
 		}
+	}
+}
+
+func TestLetStatements(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let a = 5; a;", 5},
+		{"let a = 5 * 5; a;", 25},
+		{"let a = 5; let b = a; b;", 5},
+		{"let a = 5; let b = a; let c = a + b + 5; c;", 15},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
 	}
 }
--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/ktny/monkey/object"
+
 	"github.com/ktny/monkey/evaluator"
 	"github.com/ktny/monkey/lexer"
 	"github.com/ktny/monkey/parser"
@@ -15,6 +17,7 @@ const PROMPT = ">> "
 // Start REPLの開始
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -33,7 +36,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
