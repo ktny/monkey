3b50520c1a4cc5bcc2e43f90ac2058e219bb8df8
ktny <katsunori.t10@gmail.com>
2021-01-09 21:45:00 +0900

    add hash object

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,8 @@
 package evaluator
 
 import (
+	"fmt"
+
 	"github.com/ktny/monkey/object"
 )
 
@@ -82,7 +84,7 @@ var builtins = map[string]*object.Builtin{
 				return newError("wrong number of arguments. got=%d, want=2", len(args))
 			}
 			if args[0].Type() != object.ARRAY_OBJ {
-				return newError("argument to `rest` must be ARRAY, got %s", args[0].Type())
+				return newError("argument to `push` must be ARRAY, got %s", args[0].Type())
 			}
 
 			arr := args[0].(*object.Array)
@@ -93,6 +95,15 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
--- token/token.go
+++ token/token.go
@@ -42,6 +42,7 @@ const (
 	RBRACE   = "}"
 	LBRACKET = "["
 	RBRACKET = "]"
+	COLON    = ":"
 
 	// キーワード
 	FUNCTION = "FUNCTION"
